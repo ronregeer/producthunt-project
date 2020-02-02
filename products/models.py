8f9ef1bb051df456f6a62005558ebd348c534d56
ron <rregeer@live.nl>
2020-02-02 22:34:31 +0100

    create

--- products/models.py
+++ products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.TextField()
+    pub_date = models.DateTimeField()
+    votes_total = models.IntegerField(default=1)
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    body = models.TextField()
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%e %b %Y')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        if len(self.body) > 100:
+            return self.body[:100]+' ...Read More..'
+        else:
+            return self.body
--- products/views.py
+++ products/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Product
 
 
 def home(request):
     return render(request, 'products/home.html')
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.POST['icon'] and request.POST['image'] :
+            product = Product()
+            product.title = request.POST['title']
+            product.body = request.POST['body']
+            if request.POST['url'].startswitch('http://') or request.POST['url'].startswitch('https://') :
+                product.url = request.POST['url']
+            else:
+                product.url = 'http://' + request.POST['url']
+            product.icon = request.FILES['icon']
+            product.image = request.FILES['image']
+
+    else:
+        return render(request, 'products/create.html')
